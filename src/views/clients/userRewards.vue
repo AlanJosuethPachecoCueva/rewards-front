eea4af88c67de519f9822e86d9c6737abf9d63c6
Erik Murminacho <erikmurminacho07@gmail.com>
2024-06-21 12:33:38 -0500

    Added delete account on profile

--- src/views/clients/userRewards.vue
+++ src/views/clients/userRewards.vue
@@ -22,20 +22,38 @@
           <span>{{ rewards.platinum }}</span>
         </div>
       </div>
+      <p>{{ "If you want to erase your account press this button" }}</p>
+      <button
+        type="button"
+        class="btn btn-primary btnGenerateKit"
+        @click="deleteProfile()"
+      >
+        Delete Profile
+      </button>
     </div>
     <div class="rewards">
       <h3>Recently Played</h3>
       <div class="reward-items-container">
-        <div class="reward-item" v-for="(item, index) in user.rewards" :key="index">
+        <div
+          class="reward-item"
+          v-for="(item, index) in user.rewards"
+          :key="index"
+        >
           <div v-if="item.type != '3d'">
             <img :src="item.url" alt="Reward Image" />
             <p>{{ item.metadata[0].metadata.title }}</p>
           </div>
           <div v-else>
-            <model-viewer :src="item.url" alt="Modelo 3D" disable-zoom disable-pan disable-touch-zoom disable-rotate
-              auto-rotate="0"></model-viewer>
+            <model-viewer
+              :src="item.url"
+              alt="Modelo 3D"
+              disable-zoom
+              disable-pan
+              disable-touch-zoom
+              disable-rotate
+              auto-rotate="0"
+            ></model-viewer>
           </div>
-
         </div>
       </div>
     </div>
@@ -45,23 +63,35 @@
 <script>
 import { useUserStore } from "../../stores/userStore.js";
 import { computed, reactive } from "vue";
-import { getUserByIdController, getUserRewardsController } from "../../controllers/usersController";
-import '@google/model-viewer';
+import { logOut } from "@/models/firebase/auth.js";
+import {
+  getUserByIdController,
+  getUserRewardsController,
+  deleteUserByIdController,
+} from "../../controllers/usersController";
+import "@google/model-viewer";
 
 export default {
+  data() {
+    return {
+      actualUser: null,
+    };
+  },
   components: {
-    'model-viewer': window.ModelViewer,
+    "model-viewer": window.ModelViewer,
   },
   setup() {
     const store = useUserStore();
     const userStored = computed(() => store.getUser);
+    //this.actualUser = userStored;
 
     // Creamos los datos iniciales reactivos
     const user = reactive({
-      avatar: 'default-avatar.png',
-      name: '',
-      surname: '',
-      email: '',
+      id: userStored.value.id,
+      avatar: "default-avatar.png",
+      name: "",
+      surname: "",
+      email: "",
       rewards: [],
     });
 
@@ -76,8 +106,10 @@ export default {
     const loadData = async () => {
       try {
         const userData = await getUserByIdController(userStored.value.id);
-        const rewardsWithUrls = await getUserRewardsController(userStored.value.id);
-        user.avatar = userData.avatar || 'default-avatar.png';
+        const rewardsWithUrls = await getUserRewardsController(
+          userStored.value.id
+        );
+        user.avatar = userData.avatar || "default-avatar.png";
         user.name = userData.name;
         user.surname = userData.surname;
         user.email = userData.email;
@@ -97,6 +129,42 @@ export default {
 
     // Devolvemos los datos y los métodos que queremos hacer accesibles en el template
     return { user, rewards };
+  },
+  methods: {
+    deleteProfile() {
+      this.$swal({
+        title: "¿Estás seguro de eliminar tu cuenta?",
+        text: "Todos tus datos se borrarán y no podras recuperar tus puntos",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sí, eliminarlo",
+        cancelButtonText: "Cancelar",
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          // Lógica para eliminar el kit
+          const res = await deleteUserByIdController(this.user.id);
+          if (!res) {
+            throw new Error(`User ${this.user.id} was not deleted`);
+          }
+          console.log(`Deleting user with ID: ${this.user.id}`);
+          // Aquí llamarías a tu controlador de eliminación y actualizarías la lista de kits
+          this.$swal("¡Eliminado!", "Tu cuenta ha sido eliminada", "success");
+          this.closeSession();
+        }
+      });
+    },
+    async closeSession() {
+      console.log("Close session");
+      try {
+        await logOut();
+        // this.userStatus = false;
+        window.location.reload();
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 };
 </script>
