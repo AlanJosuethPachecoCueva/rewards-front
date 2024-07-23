31bb7aa5e692c60e532992d62cd5edafd02d47e4
Erik Murminacho <erikmurminacho07@gmail.com>
2024-07-23 06:45:39 -0500

    small fix

--- src/views/clients/showRewards.vue
+++ src/views/clients/showRewards.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="containerActiveRewards">
-
-
     <div class="titleContainer">
-      <h1>{{ $t('showRewardsTitle') }}</h1>
+      <h1 class="rewards-title">{{ $t("showRewardsTitle") }}</h1>
     </div>
     <div v-if="isLoading" class="loading-container">
       <div class="spinner"></div>
@@ -12,10 +10,20 @@
 
     <div class="containerShowRewards">
       <div class="search-container">
-        <input type="text" v-model="searchQuery" :placeholder="$t('showRewardsSearchplaceholder')" class="search-bar" />
+        <input
+          type="text"
+          v-model="searchQuery"
+          :placeholder="$t('showRewardsSearchplaceholder')"
+          class="search-bar"
+        />
       </div>
 
-      <div v-for="(kitData, kitId) in filteredKitsRewards" :key="kitId" class="kit-section" id="containerOfKitRewards">
+      <div
+        v-for="(kitData, kitId) in filteredKitsRewards"
+        :key="kitId"
+        class="kit-section"
+        id="containerOfKitRewards"
+      >
         <!-- {{ kitData }} -->
         <div id="containerOfKitRewardsTitle">
           <h2>{{ kitData.kit.title }}</h2>
@@ -25,20 +33,34 @@
           <p>{{ kitData.kit.description }}</p>
         </div>
 
-
         <div v-if="kitData.rewards.stickers.length > 0">
           <h3 class="activeRewardsTitle">Stickers</h3>
           <div class="card-grid">
-            <div v-for="sticker in kitData.rewards.stickers" :key="sticker" class="cardShowRewards">
+            <div
+              v-for="sticker in kitData.rewards.stickers"
+              :key="sticker"
+              class="cardShowRewards"
+            >
               <div class="card-image-container">
-                <img :src="getRewardUrl(sticker)" class="card-img" alt="Reward Image" />
-                <button class="redeem-button" @click="reedem(sticker, 'st', kitId)">
-                  {{ $t('showRewardRedeemButton') }}
+                <img
+                  :src="getRewardUrl(sticker)"
+                  class="card-img"
+                  alt="Reward Image"
+                />
+                <button
+                  class="redeem-button"
+                  @click="reedem(sticker, 'st', kitId)"
+                >
+                  {{ $t("showRewardRedeemButton") }}
                 </button>
                 <button class="award-cost" @click="">
                   {{ getRewardCost(sticker) }}
                   <i>
-                    <img src="../../assets//images//cacao.png" alt="Points Icon" style="width: 20px; height: 20px" />
+                    <img
+                      src="../../assets//images//cacao.png"
+                      alt="Points Icon"
+                      style="width: 20px; height: 20px"
+                    />
                   </i>
                 </button>
               </div>
@@ -53,23 +75,43 @@
         <div v-if="kitData.rewards.threeDObjects.length > 0">
           <h3 class="activeRewardsTitle">3D Objects</h3>
           <div class="card-grid">
-            <div v-for="threeDObject in kitData.rewards.threeDObjects" :key="threeDObject" class="cardShowRewards">
+            <div
+              v-for="threeDObject in kitData.rewards.threeDObjects"
+              :key="threeDObject"
+              class="cardShowRewards"
+            >
               <div class="card-image-container custom-card-size">
-                <model-viewer :src="getRewardUrl(threeDObject)" alt="Modelo 3D" disable-zoom disable-pan
-                  disable-touch-zoom disable-rotate auto-rotate="0"></model-viewer>
-                <button class="redeem-button" @click="reedem(threeDObject, '3d', kitId)">
-                  {{ $t('showRewardRedeemButton') }}
+                <model-viewer
+                  :src="getRewardUrl(threeDObject)"
+                  alt="Modelo 3D"
+                  disable-zoom
+                  disable-pan
+                  disable-touch-zoom
+                  disable-rotate
+                  auto-rotate="0"
+                ></model-viewer>
+                <button
+                  class="redeem-button"
+                  @click="reedem(threeDObject, '3d', kitId)"
+                >
+                  {{ $t("showRewardRedeemButton") }}
                 </button>
                 <button class="award-cost" @click="">
                   {{ getRewardCost(threeDObject) }}
                   <i>
-                    <img src="../../assets//images//cacao.png" alt="Points Icon" style="width: 20px; height: 20px" />
+                    <img
+                      src="../../assets//images//cacao.png"
+                      alt="Points Icon"
+                      style="width: 20px; height: 20px"
+                    />
                   </i>
                 </button>
               </div>
               <div class="card-body">
                 <h5 class="card-title">{{ getRewardTitle(threeDObject) }}</h5>
-                <p class="card-text">{{ getRewardDescription(threeDObject) }}</p>
+                <p class="card-text">
+                  {{ getRewardDescription(threeDObject) }}
+                </p>
               </div>
             </div>
           </div>
@@ -78,16 +120,31 @@
         <div v-if="kitData.rewards.products.length > 0">
           <h3 class="activeRewardsTitle">Products</h3>
           <div class="card-grid">
-            <div v-for="product in kitData.rewards.products" :key="product" class="cardShowRewards">
+            <div
+              v-for="product in kitData.rewards.products"
+              :key="product"
+              class="cardShowRewards"
+            >
               <div class="card-image-container">
-                <img :src="getRewardUrl(product)" class="card-img" alt="Reward Image" />
-                <button class="redeem-button" @click="reedem(product, 'pr', kitId)">
-                  {{ $t('showRewardRedeemButton') }}
+                <img
+                  :src="getRewardUrl(product)"
+                  class="card-img"
+                  alt="Reward Image"
+                />
+                <button
+                  class="redeem-button"
+                  @click="reedem(product, 'pr', kitId)"
+                >
+                  {{ $t("showRewardRedeemButton") }}
                 </button>
                 <button class="award-cost" @click="">
                   {{ getRewardCost(product) }}
                   <i>
-                    <img src="../../assets//images//cacao.png" alt="Points Icon" style="width: 20px; height: 20px" />
+                    <img
+                      src="../../assets//images//cacao.png"
+                      alt="Points Icon"
+                      style="width: 20px; height: 20px"
+                    />
                   </i>
                 </button>
               </div>
@@ -98,14 +155,18 @@
             </div>
           </div>
         </div>
-
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { getStickersController, get3DObjectsController, getProductsImagesController, getKitsRewardsController } from "../../controllers/rewardsController";
+import {
+  getStickersController,
+  get3DObjectsController,
+  getProductsImagesController,
+  getKitsRewardsController,
+} from "../../controllers/rewardsController";
 import "@google/model-viewer";
 
 export default {
@@ -129,18 +190,31 @@ export default {
         const kit = kitData.kit;
         const rewards = kitData.rewards;
 
-        if (kit.title.toLowerCase().includes(query) || kit.description.toLowerCase().includes(query)) {
+        if (
+          kit.title.toLowerCase().includes(query) ||
+          kit.description.toLowerCase().includes(query)
+        ) {
           filteredKits[kitId] = kitData;
           continue;
         }
 
         const filteredRewards = {
-          stickers: rewards.stickers.filter((sticker) => this.getRewardTitle(sticker).toLowerCase().includes(query)),
-          threeDObjects: rewards.threeDObjects.filter((threeDObject) => this.getRewardTitle(threeDObject).toLowerCase().includes(query)),
-          products: rewards.products.filter((product) => this.getRewardTitle(product).toLowerCase().includes(query)),
+          stickers: rewards.stickers.filter((sticker) =>
+            this.getRewardTitle(sticker).toLowerCase().includes(query)
+          ),
+          threeDObjects: rewards.threeDObjects.filter((threeDObject) =>
+            this.getRewardTitle(threeDObject).toLowerCase().includes(query)
+          ),
+          products: rewards.products.filter((product) =>
+            this.getRewardTitle(product).toLowerCase().includes(query)
+          ),
         };
 
-        if (filteredRewards.stickers.length || filteredRewards.threeDObjects.length || filteredRewards.products.length) {
+        if (
+          filteredRewards.stickers.length ||
+          filteredRewards.threeDObjects.length ||
+          filteredRewards.products.length
+        ) {
           filteredKits[kitId] = { kit, rewards: filteredRewards };
         }
       }
@@ -188,6 +262,8 @@ export default {
 <style>
 #containerActiveRewards {
   background: rgba(0, 0, 0, 0.849);
+  background: url(/src/assets/images/banner_hor.png) no-repeat center center
+    fixed;
 }
 
 .activeRewardsTitle {
@@ -201,7 +277,6 @@ export default {
 }
 
 #containerOfKitRewards {
-
   margin: 40px;
   padding: 50px;
   border-radius: 30px;
@@ -216,7 +291,7 @@ export default {
 
 #containerOfKitRewardsTitle {
   display: flex;
-  
+
   color: white;
   height: fit-content;
   justify-content: center;
@@ -232,22 +307,21 @@ export default {
   height: fit-content;
   justify-content: center;
   align-items: center;
-  
-  font-weight:bolder !important;
+
+  font-weight: bolder !important;
 
   border-radius: 10px;
 }
-
-
 
 .titleContainer {
   /* background-color: red; */
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 60px 0 40px 0;
+  padding: 60px 0 10px 0;
   color: white;
   font-weight: bolder;
+  font-size: xx-large;
 }
 
 .containerShowRewards {
@@ -287,7 +361,6 @@ export default {
   overflow: hidden;
   position: relative;
   transition: transform 0.2s;
-
 }
 
 .cardShowRewards:hover {
@@ -353,7 +426,11 @@ export default {
   font-size: 14px;
   color: #777;
 }
-
+.rewards-title {
+  font-weight: bolder;
+  font-size: 2.5rem;
+  margin-bottom: 0 !important;
+}
 
 /* Estilos para la animación de carga */
 .loading-container {
@@ -375,7 +452,7 @@ export default {
 .spinner {
   border: 8px solid #f3f3f3;
   /* Light grey */
-  border-top: 8px solid #CDA434;
+  border-top: 8px solid #cda434;
   /* Blue */
   border-radius: 50%;
   width: 60px;
